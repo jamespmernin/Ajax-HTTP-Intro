dc1208a67eff8883ef026bd960b15c5b59fca49e
James Mernin <jamespmernin@gmail.com>
2020-09-17 14:30:56 -0400

    Finished lesson

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,18 +1,42 @@
 const getData = () => {
+  const headers = {
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  }
+  removePerson();
   const url = "https://cors-anywhere.herokuapp.com/https://randomuser.me/api/?results=10";
-  axios.get(url).then((res) => {
+  axios.get(url, headers).then((res) => {
     const response = res.data.results;
     // console.log(response);
     const peopleDiv = document.querySelector('.people');
     response.forEach((person) => {
-      console.log(person.name.first);
+      // console.log(person.picture.large);
       const personDiv = document.createElement('div');
+      personDiv.classList = 'person-div';
       peopleDiv.append(personDiv);
+      const name = document.createElement('p');
       const firstName = person.name.first;
+      const lastName = person.name.last;
+      name.textContent = `${firstName} ${lastName}`;
+      personDiv.append(name);
+      const img = document.createElement('img');
+      img.src = person.picture.large;
+      personDiv.append(img);
     });
   }).catch((error) => {
     console.log(`Error: ${error}`);
+  }).finally(() => {
+    console.log('Look at the beautiful people.');
   });
 }
 
-getData();
+const button = document.querySelector('button');
+button.addEventListener('click', getData);
+
+function removePerson() {
+  const removeDiv = document.querySelector('.people');
+  while (removeDiv.lastChild) {
+    removeDiv.removeChild(removeDiv.lastChild);
+  }
+}
